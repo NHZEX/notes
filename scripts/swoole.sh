97629b1a208a3690288e355af8781270fccbbe93
auooru <auooru@outlook.com>
2020-01-05 17:54:41 +0800

    update swoole.sh

--- scripts/swoole.sh
+++ scripts/swoole.sh
@@ -22,15 +22,19 @@ downSwoole () {
     sha1sum -b ${SWOOLE_TAR} > ${SWOOLE_SUM}
 }
 
-echo "php      : ${PHP_VER}"
-echo "swoole   : ${SWOOLE_VER}"
-
 SWOOLE_DIR="${INSTALL_DIR}/swoole"
 SWOOLE_TAR="${SWOOLE_DIR}/v${SWOOLE_VER}.tar.gz"
 SWOOLE_SUM="${SWOOLE_DIR}/v${SWOOLE_VER}.sha1"
 SWOOLE_MAKE_DIR="${SWOOLE_DIR}/v${SWOOLE_VER}"
-SWOOLE_INI="/etc/php/${PHP_VER}/cli/conf.d/30-swoole.ini"
+SWOOLE_INI="30-swoole.ini" # /etc/php/${PHP_VER}/cli/conf.d/
 
+PHP_INI_FILE=`php${PHP_VER} -r "echo php_ini_loaded_file();"`
+PHP_SCAN_DIR=`php${PHP_VER} -r "echo dirname(substr(@php_ini_scanned_files(), 0, strpos(@php_ini_scanned_files(), ',')));"`
+
+echo "php ver  : ${PHP_VER}"
+echo "php ini  : ${PHP_INI_FILE}"
+echo "php ini2 : ${PHP_SCAN_DIR}"
+echo "swoole   : ${SWOOLE_VER}"
 echo "base dir : ${SWOOLE_DIR}"
 echo "make dir : ${SWOOLE_MAKE_DIR}"
 echo "tar file : ${SWOOLE_TAR}"
@@ -59,8 +63,19 @@ phpize${PHP_VER}
 make -j$(nproc)
 sudo make install
 
-if [[ ! -f ${SWOOLE_INI} ]]; then
-    echo "extension=swoole.so" | sudo tee ${SWOOLE_INI}
+set +e
+php${PHP_VER} --ri swoole
+
+if [[ $? -eq 0 ]]; then
+    exit 0
+fi
+
+if [[ -d ${PHP_SCAN_DIR} ]]; then
+    if [[ ! -f ${SWOOLE_INI} ]]; then
+        echo "extension=swoole.so" | sudo tee "${PHP_SCAN_DIR}/${SWOOLE_INI}"
+    fi
+else
+    sudo sed -e '0,/\[PHP\]/ a\\nextension=swoole.so\n\n' ${PHP_INI_FILE}
 fi
 
 php${PHP_VER} --ri swoole
